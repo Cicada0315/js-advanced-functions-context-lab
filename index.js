9bdfff3c812a0a65b3a26aa448051b695eec1d0f
jean <jin.ahn.1027@gmail.com>
2021-05-13 16:32:01 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,67 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(arr){
+    let obj={
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj;
+}
+function createEmployeeRecords(arr){
+    let obj=[];
+    for(let i=0; i<arr.length; i++){
+        obj.push(createEmployeeRecord(arr[i]))
+    }
+    return obj;
+}
+function creatTimeObj(type, time){
+    const timearr=time.split(" ");
+    return {
+        type: type,
+        date: timearr[0],
+        hour: parseInt(timearr[1])
+    }
+}
+
+function createTimeInEvent(time){
+    this.timeInEvents.push(creatTimeObj("TimeIn", time));
+    return this;
+}
+
+function createTimeOutEvent(time){
+    this.timeOutEvents.push(creatTimeObj("TimeOut", time));
+    return this;
+}
+
+function hoursWorkedOnDate(date){
+    const indobj = this.timeInEvents.find(e => e.date===date);
+    const outobj= this.timeOutEvents.find(e=> e.date===date);
+
+    return (outobj.hour-indobj.hour)/100;
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date)*this.payPerHour;
+}
+
+function calculatePayroll(employee){
+    let grandTotalOwed=0;
+    for(let i=0; i<employee.length; i++){
+        grandTotalOwed+=allWagesFor.call(employee[i])
+    }
+    return grandTotalOwed;
+}
+
+function findEmployeeByFirstName(records, name){
+    for(let i=0; i<records.length; i++){
+        if(records[i].firstName===name){
+            return records[i];
+        }
+    }
+}
